a9a93c3290f35e23f49c519502d8f59fedaf526a
muktaraut2601-byte <muktaraut2601@gmail.com>
2026-09-07 15:08:02 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,17 +1,17 @@
+package codealpha_studentgradetracker;
+
 public class Student {
 
     private int id;
     private String name;
     private double marks;
 
-    // Constructor
     public Student(int id, String name, double marks) {
         this.id = id;
         this.name = name;
         this.marks = marks;
     }
 
-    // Getters
     public int getId() {
         return id;
     }
@@ -24,18 +24,11 @@ public class Student {
         return marks;
     }
 
-    // Setters
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setMarks(double marks) {
         this.marks = marks;
     }
 
-    // Calculate Grade
     public String getGrade() {
-
         if (marks >= 90) {
             return "A+";
         } else if (marks >= 80) {
@@ -51,16 +44,11 @@ public class Student {
         }
     }
 
-    // Display student information
-    @Override
-    public String toString() {
-        return "ID: " + id
-                + " | Name: " + name
-                + " | Marks: " + marks
-                + " | Grade: " + getGrade();
+    public void display() {
+        System.out.printf("%-10d %-20s %-12.2f %-10s%n",
+                id, name, marks, getGrade());
     }
 
-    // Convert student data into file format
     public String toFileString() {
         return id + "," + name + "," + marks;
     }
